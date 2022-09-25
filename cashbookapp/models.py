dfcc7ea18091c38e74d551b67421472cd2d88717
i1000u <grandromantic77@gmail.com>
2022-09-25 20:02:08 +0900

    cr

--- cashbookapp/models.py
+++ cashbookapp/models.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from datetime import datetime
 import email
 from pickle import TRUE
@@ -5,7 +6,8 @@ from pyexpat import model
 from re import T
 from venv import create
 from django.db import models
-
+from django.core.validators import MinLengthValidator
+from django.core.exceptions import ValidationError
 # Create your models here.
 
 
@@ -13,12 +15,13 @@ class Cashbook(models.Model):
     title = models.CharField(max_length=200)
     # pub_date = models.DateTimeField('data published')
     content = models.TextField()
-    ##################################################
     email = models.EmailField(max_length=254, blank=True)
     url = models.URLField(max_length=200, blank= True)
     created_at = models.DateTimeField(default= datetime.now, blank=True)
 
     def __str__(self):
         return self.title
+#####################################################
+
 
 
